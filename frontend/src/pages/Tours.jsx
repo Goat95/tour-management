da070ee688ebee5c2f39e4730babc4fb048627e5
Goat95 <hjh9703@naver.com>
2023-07-23 00:22:05 +0900

    feat: implements tours component and common section

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "reactstrap";
+
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
+
+import CommonSection from "../shared/CommonSection";
+import TourCard from "../shared/TourCard";
+import SearchBar from "../shared/SearchBar";
+import Newsletter from "../shared/Newsletter";
+
 const Tours = () => {
-  return <div>Tours</div>;
+  const [pageCount, setPageCount] = useState(0);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    const pages = Math.ceil(5 / 4); // later we will use backend data count
+    setPageCount(pages);
+  }, [page]);
+
+  return (
+    <>
+      <CommonSection title={"All Tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+            <Col lg="12">
+              <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
+                {[...Array(pageCount).keys()].map((number) => (
+                  <span
+                    key={number}
+                    onClick={() => setPage(number)}
+                    className={page === number ? "active__page" : ""}
+                  >
+                    {number + 1}
+                  </span>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <Newsletter />
+    </>
+  );
 };
 
 export default Tours;
